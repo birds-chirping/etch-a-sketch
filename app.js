333a8d9ca895e33e4a204712215f9e5b50817dc3
Birds Chirping <afara.ninge22@gmail.com>
2022-10-25 20:27:46 +0300

    Add clear button

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const gridContainer = document.querySelector('.grid-container');
-
-
-// grid size at start:
-let gridSize = 64;
-
+const clearBtn = document.getElementById('clear');
 let squares = [];
-let color = '#FF0000';
+
+// starting settings
+let gridSize = 30;
+let selectedColor = '#FF0000';
+
 
 function createGrid(size) {
     let id = 1;
-    for (let row = 0; row < gridSize; row++) {
+    for (let row = 0; row < size; row++) {
         const gridRow = document.createElement('div');
         gridRow.setAttribute('class', 'grid-row');
     
-        for (let col = 0; col < gridSize; col++) {
+        for (let col = 0; col < size; col++) {
             const square = document.createElement('div');
             square.setAttribute('class','square');
             square.setAttribute('id', `${id}`);
@@ -21,23 +21,31 @@ function createGrid(size) {
             gridRow.appendChild(square);
             id++;
         }
-    
         gridContainer.appendChild(gridRow);
     }  
 }
 
-function changeSquareColor(){
-        // console.log(this);
-        this.setAttribute('style', `background-color: ${color}`);
+
+function changeSquareColor(square, color) {
+        square.setAttribute('style', `background-color: ${color}`);
+}
+
+function clearCanvas() {
+    squares.forEach(square => changeSquareColor(square,'#FFF'));
 }
 
 
-
+//-------------------------------------------------------------------
 createGrid(gridSize);
 
+//----------------------------event listeners------------------------
 squares.forEach(square => {
-    square.addEventListener('mouseover', changeSquareColor);
+    square.addEventListener('mouseover',function() {
+        changeSquareColor(square, selectedColor)   
+    })
 });
+
+clearBtn.addEventListener('click', clearCanvas);
 
 
 
