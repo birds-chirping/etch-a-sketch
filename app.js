f75588d7434d7ebc42048e8f1b5881e891dea78f
Birds Chirping <afara.ninge22@gmail.com>
2022-10-27 00:29:11 +0300

    Refactor code.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Get the root element
+let r = document.querySelector(':root');
+let rs = getComputedStyle(r);
+
 // grid
 const gridContainer = document.querySelector('.grid-container');
 
@@ -18,7 +22,6 @@ const satSlider = document.getElementById('sat-range');
 const satValue = document.getElementById('sat-value');
 const lightSlider = document.getElementById('light-range');
 const lightValue = document.getElementById('light-value');
-
 const colorCanvas = document.getElementById('chosen-hue');
 
 
@@ -28,15 +31,13 @@ let selectedColor = '#FF0000';
 let squares = [];
 let random = false;
 let hue = 0;
-let saturation = 100;
-let lightness = 50;
-
+let saturation = '100%';
+let lightness = '50%';
 
 
 
 function createGrid(size) {
     gridContainer.textContent = '';
-
     let id = 1;
     for (let row = 0; row < size; row++) {
         const gridRow = document.createElement('div');
@@ -52,10 +53,8 @@ function createGrid(size) {
         }
         gridContainer.appendChild(gridRow);
     } 
-
     changeColorOnHover();
 }
-
 
 function changeColorOnHover() {
     squares.forEach(square => {
@@ -72,10 +71,14 @@ function changeColorOnHover() {
     };
 }
 
-
 function changeSquareColor(square, color=getColor()) {
     square.setAttribute('style', `background-color: ${color}`);
 }
+
+function getColor() {
+    random === false ? selectedColor : selectedColor = pickRandomColor(); 
+    return selectedColor;
+} 
 
 function pickRandomColor() {
     let h = Math.floor(Math.random() * 360);
@@ -85,14 +88,20 @@ function pickRandomColor() {
     return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
+//----------------------CSS variables:
+function changeColorValues(param, value) {
+    r.style.setProperty(param, value);
+  }
 
-function getColor() {
-    random === false ? selectedColor : selectedColor = pickRandomColor(); 
-    return selectedColor;
-} 
+function getLastUsedColor() {
+    let h = rs.getPropertyValue('--hue');
+    let s = rs.getPropertyValue('--sat');
+    let l = rs.getPropertyValue('--light');
+    return `hsl(${h}, ${s}, ${l})`;
+}
 
 //-------------------------------------------------------------------
-//-------------------------------------------------------------------
+//---------------------------APP-------------------------------------
 
 createGrid(gridSize);
 
@@ -105,7 +114,7 @@ clearBtn.addEventListener('click', function() {
     squares.forEach(function(e) {
         changeSquareColor(e);
     });
-    selectedColor = '#FF0000';
+    selectedColor = getLastUsedColor();
 });
 
 //-------------random button:
@@ -116,9 +125,9 @@ randomBtn.addEventListener('click', function() {
 //-------------default button:
 defaultBtn.addEventListener('click', function() {
     random = false;
-    selectedColor = '#FF0000';
+    selectedColor = getLastUsedColor();
+    
 });
-
 
 //-------------erase button:
 eraseBtn.addEventListener('click', function() {
@@ -126,7 +135,6 @@ eraseBtn.addEventListener('click', function() {
     selectedColor = '#FFFFFF';
 });
 
-    
 //------------size slider:
 sizeSlider.addEventListener('change', function() {
     gridSize = this.value;
@@ -134,49 +142,31 @@ sizeSlider.addEventListener('change', function() {
     createGrid(gridSize);
 })
 
-
 //------------hue slider:
 hueSlider.addEventListener('change', function() {
     hueValue.textContent = this.value; 
     hue = this.value;
-    colorCanvas.setAttribute('style', `background-color: hsl(${hue}, ${saturation}%, ${lightness}%)`);
-    selectedColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-    satSlider.style.backgroundImage = `linear-gradient(to right, 
-        hsl(${hue}, 0%, ${lightness}%),
-        hsl(${hue}, 100%, ${lightness}%))`;
-
-    lightSlider.style.backgroundImage = `linear-gradient(to right, 
-        hsl(0, ${saturation}%, 0%),
-        hsl(${hue}, ${saturation}%, 50%),
-        hsl(0, ${saturation}%, 100%))`;
+    colorCanvas.setAttribute('style', `background-color: hsl(${hue}, ${saturation}, ${lightness})`);
+    selectedColor = `hsl(${hue}, ${saturation}, ${lightness})`;
+    changeColorValues('--hue', hue);        
 })
 
 //-----------saturation slider:
-
 satSlider.addEventListener('change', function() {
     satValue.textContent = this.value; 
-    saturation = this.value;
-    colorCanvas.setAttribute('style', `background-color: hsl(${hue}, ${saturation}%, ${lightness}%)`);    
-    selectedColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-
-    lightSlider.style.backgroundImage = `linear-gradient(to right, 
-        hsl(0, ${saturation}%, 0%),
-        hsl(${hue}, ${saturation}%, 50%),
-        hsl(0, ${saturation}%, 100%))`;
+    saturation = `${this.value}%`;
+    colorCanvas.setAttribute('style', `background-color: hsl(${hue}, ${saturation}, ${lightness})`);    
+    selectedColor = `hsl(${hue}, ${saturation}, ${lightness})`;
+    changeColorValues('--sat', saturation);        
 })
 
 //-----------lightness slider:
-
 lightSlider.addEventListener('change', function() {
     lightValue.textContent = this.value; 
-    lightness = this.value;
-    colorCanvas.setAttribute('style', `background-color: hsl(${hue}, ${saturation}%, ${lightness}%)`);    
-    selectedColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-
-    satSlider.style.backgroundImage = `linear-gradient(to right, 
-        hsl(${hue}, 0%, ${lightness}%),
-        hsl(${hue}, 100%, ${lightness}%))`;
-
+    lightness = `${this.value}%`
+    colorCanvas.setAttribute('style', `background-color: hsl(${hue}, ${saturation}, ${lightness})`);    
+    selectedColor = `hsl(${hue}, ${saturation}, ${lightness})`;
+    changeColorValues('--light', lightness);        
 })
 
 
